7709cb312dafa76b31c784fd8b4a3ff6e1532f9c
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-09-14 03:06:43 +0700

    initial commit v2

---
+++ src/components/TipCalculator.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import IconDollar from "./IconDollar.vue";
+</script>
+
+<template>
+  <div class="rounded-t-3xl bg-white p-8 text-neutral-dark-gray-cyan">
+    <label class="flex flex-col">
+      <span>Bill</span>
+    </label>
+  </div>
+</template>
---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(172, 67%, 45%)",
+        },
+        neutral: {
+          "dark-cyan": "hsl(183, 100%, 15%)",
+          "dark-gray-cyan": "hsl(186, 14%, 43%)",
+          "gray-cyan": "hsl(184, 14%, 56%)",
+          "light-gray-cyan": "hsl(185, 41%, 84%)",
+          "lighter-gray-cyan": "hsl(189, 41%, 97%)",
+        },
+      },
+      fontFamily: {
+        "space-mono": ["Space Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
